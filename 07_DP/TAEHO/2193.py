e150cdbb83e6aa4609ea839578700bf2e21ae67c
Train0303 <rjsdnxogh55@gmail.com>
2022-01-13 11:45:30 +0900

    taeho 01-13

---
+++ 07_DP/TAEHO/2193.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+n = int(stdin.readline())
+if n == 1:
+    print(1)
+else:
+    DP = [0]*(n+1)
+    DP[1] = 1
+    DP[2] = 1
+    for i in range(3,n+1):
+        DP[i] = DP[i-1] + DP[i-2]
+    print(DP[n])
