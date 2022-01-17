89e4e7a4a0f3ff1e2c41379dec6f58e47c3f2b22
Train0303 <rjsdnxogh55@gmail.com>
2022-01-17 12:27:59 +0900

    taeho 01-17

---
+++ 09_Bellman-Ford/1865.py
@@ -0,0 +1,30 @@
+from sys import stdin,maxsize
+
+def bellmanFord():
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            for wei,vec in graph[j]:
+                if dist[vec] > wei + dist[j]:
+                    dist[vec] = wei + dist[j]
+                    if i == n:
+                        return False
+    return True
+
+tc = int(stdin.readline())
+
+for _ in range(tc):
+    n,m,w = map(int, stdin.readline().split())
+    dist = [maxsize for _ in range(n+1)]
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        s,e,t = map(int, stdin.readline().split())
+        graph[s].append((t,e))
+        graph[e].append((t,s))
+    
+    for _ in range(w):
+        s,e,t = map(int,stdin.readline().split())
+        graph[s].append((-t,e))
+    
+    result = bellmanFord()
+
+    print("NO" if result else "YES")
