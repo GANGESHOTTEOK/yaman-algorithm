e85ded7269f76d9391ae26f70a2f69661a942157
muzee99 <muzee99@pusan.ac.kr>
2022-01-24 12:16:09 +0900

    modify BOJ11758

--- 14_Geometry/AN/BOJ11758.py
+++ 14_Geometry/AN/BOJ11758.py
@@ -4,12 +4,11 @@ x1,y1 = map(int, input().split())
 x2,y2 = map(int, input().split())
 x3,y3 = map(int, input().split())
 
-a = (y2-y1)*(x3-x1)+y1*(x2-x1)
-b = (x2-x1)*y3
+CCW = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
 
-if a<b:
+if CCW>0:
     print(1)
-elif a>b:
+elif CCW<0:
     print(-1)
 else:
     print(0)
