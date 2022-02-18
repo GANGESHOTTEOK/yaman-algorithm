e81b0c761910b118ef1aaf001a5b8805a4df72ba
muzee99 <muzee99@pusan.ac.kr>
2022-02-18 16:32:33 +0900

    BOJ17406

--- 22_Weekly_7_Problmes/AN/BOJ17281.py
+++ 22_Weekly_7_Problmes/AN/BOJ17281.py
@@ -2,28 +2,35 @@ import sys
 from itertools import permutations
 input = sys.stdin.readline
 
+# get score with line_up
 def play(line_up):
     score = turn = 0
-    line_up.insert(3,0)
+    line_up.insert(3,0)         # fourth hitter is fixed
+
     for inning in innings:
-        out = 0
-        runners = []
+        out = 0   # out count
+        runners = []   # runners' result in this inning
+
         while out<3:
             hitter = line_up[turn]
-            if not inning[hitter]:
-                out += 1
-            else:
-                runners.append(inning[hitter])
-            turn = (turn+1)%9
+            if not inning[hitter]:  # if result is 0
+                out += 1            # out count increase
+            else:                   # if result is not 0
+                runners.append(inning[hitter])  # add runners
+            turn = (turn+1)%9       # next hitter
+        
+        # find rest of runners on base, cannot get score
         base = 0
         for i in range(len(runners)-1,len(runners)-4,-1):
-            if i < 0:
+            if i < 0:    # out of range
                 break
             base += runners[i]
             if base >= 4:
                 break
             runners.pop()
+            
         score += len(runners)
+        
     return score
 
 N = int(input())
@@ -34,7 +41,7 @@ for _ in range(N):
 max_result = 0
 players = [1,2,3,4,5,6,7,8]
 
-for line_up in permutations(players):
+for line_up in permutations(players):    # make line-up
     max_result = max(max_result, play(list(line_up)))
 
 print(max_result)
