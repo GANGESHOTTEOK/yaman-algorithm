831bad0e2a132d4ec255bb30010572695e8ac091
WonJune Choi <55697983+asd147asd147@users.noreply.github.com>
2022-02-22 19:20:08 +0900

    Update 프렌즈4블록.py

--- 23_programmers_2018_kakao_blind_recruitment/TAEHO/프렌즈4블록.py
+++ 23_programmers_2018_kakao_blind_recruitment/TAEHO/프렌즈4블록.py
@@ -1,41 +1,40 @@
-locas = []
+from queue import Queue
 
-def check(y,x,board,friend):
-    temp = []
-    for i in range(y,y+2):
-        for j in range(x,x+2):
-            if board[i][j] != friend:
-                
-                return False
-            temp.append((i,j))
-    locas.append(temp)
-    return True
-
-def solution(m, n, board):
-    board = [list(x) for x in board]
+def fillQueue(M, N, idx):
+    q = Queue()
+    for m in range(M):
+        if(b[M-1-m][idx] == 'X'): continue
+        q.put((M-1-m, idx))
+    return q
     
-    while True:
-        for i in range(m-1):
-            for j in range(n-1):
-                if board[i][j] != '0':
-                    check(i,j,board,board[i][j])
-        
-        if locas == []:
-            break
-        
-        while locas:
-            loca = locas.pop()
-            for y,x in loca:
-                board[y][x] = '0'
-        
-        for j in range(n):
-            blank = 0
-            for i in range(m-1,-1,-1):
-                if board[i][j] == '0':
-                    blank += 1
-                else:
-                    board[i+blank][j] = board[i][j]
-                    if blank != 0:
-                        board[i][j] = '0'
-                        
-    return sum(map(lambda x : x.count('0') ,board))
+def check(q1, q2):
+	s = set()
+	if q1.qsize() == 0 or q2.qsize() == 0:
+		return s
+	block = [[q1.get(),q2.get()],['','']]
+    
+	while q1.qsize() and q2.qsize():
+		block[1][0], block[1][1] = block[0][0], block[0][1]
+		block[0][0], block[0][1] = q1.get(), q2.get()
+		if b[block[0][0][0]][block[0][0][1]] == b[block[0][1][0]][block[0][1][1]] == b[block[1][0][0]][block[1][0][1]] == b[block[1][1][0]][block[1][1][1]]:
+			s.add(block[0][0])
+			s.add(block[0][1])
+			s.add(block[1][0])
+			s.add(block[1][1])
+	return s
+    
+def solution(m, n, board):
+	answer = 0
+	global b
+	b = [list(x) for x in board]
+	while True:
+		s = set()
+		for j in range(n-1):
+			s = s | check(fillQueue(m,n,j), fillQueue(m,n,j+1))
+		if not bool(s):
+			break
+		answer += len(s)
+		for y,x in s:
+			b[y][x] = 'X'
+    
+	return answer
